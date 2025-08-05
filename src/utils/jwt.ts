1d1186bce7a8d76673824fb6dab8cc60c43d00c0
Paul <paulprem.kumar92@gmail.com>
2025-08-05 14:47:35 +0530

    intial file moved to main

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const generateToken: any = (payload: object, expiresIn = '1d') => {
