d80af758fa6b9b2d85b63a3c630c524775a7c47a
Paul <paulprem.kumar92@gmail.com>
2026-01-13 16:07:48 +0530

    port changes liston

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
